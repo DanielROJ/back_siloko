2eeadb4778250231ac3e822c4b61155d357afd8d
Daniel Rojas <danielroj.dev1@gmail.com>
2021-03-12 14:44:05 -0500

    Se reconfigura el repositorio de nuevo

---
+++ backSiloko/src/main/java/com/co/app/sb/BackSilokoApplication.java
@@ -0,0 +1,13 @@
+package com.co.app.sb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackSilokoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackSilokoApplication.class, args);
+	}
+
+}
---
+++ backSiloko/src/test/java/com/co/app/sb/BackSilokoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.co.app.sb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackSilokoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
