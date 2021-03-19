bd6dc598af20539b0e4a097f8dc6b5d6fb6a3870
Daniel Rojas <danielroj.dev1@gmail.com>
2021-03-19 02:37:44 -0500

    se siguen construyendo mappers

---
+++ backSiloko/src/main/java/com/co/app/sb/Mappers/AlmacenConvenioMapper.java
@@ -0,0 +1,24 @@
+package com.co.app.sb.Mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+import org.mapstruct.NullValueMappingStrategy;
+import org.mapstruct.ReportingPolicy;
+
+import com.co.app.sb.DTOs.AlmacenConvenioDto;
+import com.co.app.sb.model.AlmacenConvenio;
+
+@Mapper(componentModel = "spring", 
+nullValueMappingStrategy = NullValueMappingStrategy.RETURN_NULL, unmappedTargetPolicy = ReportingPolicy.IGNORE)
+public interface AlmacenConvenioMapper {
+	
+	@Mappings({
+		@Mapping(source = "idAlmacen", target = "id"),
+		@Mapping(source = "nit", target = "codeNit"),
+		@Mapping(source = "nombreAlmacen", target = "name"),
+		@Mapping(source = "pais", target = "country")
+	})
+	AlmacenConvenioDto toDto(AlmacenConvenio almacen);
+
+}
---
+++ backSiloko/src/main/java/com/co/app/sb/Mappers/ProductoCreditoMapper.java
@@ -0,0 +1,8 @@
+package com.co.app.sb.Mappers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public interface ProductoCreditoMapper {
+
+}
